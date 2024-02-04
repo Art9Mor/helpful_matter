5ca1a4818672c5310c6c18e7b6944211b13c7ce2
Artem <deusestmaleficus@gmail.com>
2024-02-05 00:39:50 +0700

    update users model

--- users/models.py
+++ users/models.py
@@ -17,11 +17,12 @@ class User(AbstractUser):
     username = None
     email = models.EmailField(unique=True, verbose_name='Почта')
     full_name = models.CharField(max_length=200, verbose_name='ФИО')
-    phone = models.SmallIntegerField(max_length=11, verbose_name='Номер телефона', **NULLABLE)
+    phone = models.CharField(max_length=11, verbose_name='Номер телефона', **NULLABLE)
     photo = models.ImageField(verbose_name='Фото', upload_to='user/', **NULLABLE)
     town = models.CharField(max_length=50, verbose_name='Город', **NULLABLE)
     status = models.CharField(max_length=150, verbose_name='Статус пользователя', default='Статус неопределён', choices=STATUS_CHOICE)
     is_active = models.BooleanField(default=True, verbose_name='Активирован')
+    token = models.CharField(max_length=12, verbose_name='код', **NULLABLE)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
